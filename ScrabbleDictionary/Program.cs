5bfb84925e3bc475cb1d7fd5e0adedbd5c7d0232
Donnyves Laroque <donnyves@gmail.com>
2019-04-01 08:56:54 -0700

    Added a Scrabble dictionary.

---
+++ ScrabbleDictionary/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScrabbleDictionary
+{
+    public class Scrabble
+    {
+        static readonly Dictionary<char, int> scrabble = new Dictionary<char, int>
+        {
+            { 'A',1},{ 'B',3},{ 'C',3},{ 'D',2},{ 'E',1},{ 'F',4},
+            { 'G',2},{ 'H',4},{ 'I',1},{ 'J',8},{ 'K',5},{ 'L',1},
+            { 'M',3},{ 'N',1},{ 'O',1},{ 'P',3},{ 'Q',10},{ 'R',1},
+            { 'S',1},{ 'T',1},{ 'U',1},{ 'V',4},{ 'W',4},{ 'X',8},
+            { 'Y',4},{ 'Z',10},
+        };
+
+
+        public static int GetScrabblePoints(string input)
+        {
+            int total = 0;
+            input = input.ToUpper();
+            foreach (char c in input)
+            {
+                total += scrabble[c];
+            }
+
+            return total;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(GetScrabblePoints ("Donnyves"));
+            Console.ReadLine();
+        }
+    }
+}
