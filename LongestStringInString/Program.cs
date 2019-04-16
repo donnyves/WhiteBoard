313a9c9acf36d02aa60aa47926e34dacd3cc73e0
Donnyves Laroque <donnyves@gmail.com>
2019-04-16 09:19:54 -0700

    Added another white board exercise.

---
+++ LongestStringInString/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace LongestStringInString
+{
+    //I'm still trying to work this problem out.  I want print out the word that has the most characters.
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            string[] array = { "Donnyves", "Ted", "Shirley", "Betty" };
+            LongestStringArray(array);
+            Console.ReadLine();
+        }
+        public static void LongestStringArray(string[] inputArray)
+        {
+            int counter = 0;
+            int max = 0;
+            int i = 0;
+            for (i = 0; i < inputArray.Length; i++)
+            {
+                if (inputArray[i].Length > max)
+                {
+                    counter = 0;
+                    max = inputArray[i].Length;
+                    counter++;
+                    continue;
+                }
+                if (inputArray[i].Length == max)
+                {
+                    counter++;
+                }
+            }
+
+            foreach (string VARIABLE in inputArray)
+            {
+                Console.Write(VARIABLE);
+            }
+
+            string[] longest = new string[counter];
+            counter = 0;
+            for (i = 0; i < inputArray.Length; i++)
+            {
+                if (inputArray[i].Length == max)
+                {
+                    longest[counter] = inputArray[i];
+                    counter++;
+                }
+            }
+
+        }
+    }
+}
